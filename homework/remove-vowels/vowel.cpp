1b087903f8082fb6616c6c58452f89d498bbbffd
kp889 <krzysztof.x.pietras@gmial.com>
2024-05-07 21:17:01 +0200

    Basic functionality

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,20 @@
+#include "vowel.hpp"
+
+void removeVowels(std::vector<std::string> &v)
+ {std::vector<char> vowels{'a','e','o','u','i','y'};
+  
+  
+    //for(auto v_it:v)
+  for (auto &element:v)
+      { for(   auto x: vowels)
+      {
+        element.erase(remove(element.begin(),element.end(),x),element.end());
+        x=toupper(x);
+        element.erase(remove(element.begin(),element.end(),x),element.end());
+      }
+        //v.erase(std::remove(v.begin(),v.end(), [](){return if(std::string::npos !=)}));
+        //v.push_back("uuu");
+      }
+
+ }
+ 
---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <deque>
+void removeVowels(std::vector<std::string> &v);
