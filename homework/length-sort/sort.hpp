c9e8bb9fa3baaf9944ea3a05ad570c0bdf74ddd3
kp889 <krzysztof.x.pietras@gmial.com>
2024-05-08 20:13:52 +0200

    basic functionality

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <algorithm>
+#include <deque>
+#include <forward_list>
+#include <string>
+#include <vector>
+std::deque<std::string> lengthSort(std::forward_list<std::string> const lista);
