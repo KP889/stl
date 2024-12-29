917d8288e0ddd74af44a3f206e9e79357fd36ac0
kp889 <krzysztof.x.pietras@gmial.com>
2024-12-29 07:52:46 +0100

    clang formating issue

--- homework/insensitive-palindrom/test.cpp
+++ homework/insensitive-palindrom/test.cpp
@@ -20,7 +20,8 @@ TEST_P(InsensitivePalindromFixture, CheckPalindrome) {
 INSTANTIATE_TEST_SUITE_P(
     InsensitivePalindromTests, InsensitivePalindromFixture,
     ::testing::Values(
-        std::make_pair("a", true), std::make_pair("aba", true),
+        std::make_pair("a", true),
+        std::make_pair("aba", true),
         std::make_pair("abba", true),
         std::make_pair("Eva, can I see bees in a cave?", true),
         std::make_pair("No lemon, no melon", true),
@@ -40,7 +41,8 @@ INSTANTIATE_TEST_SUITE_P(
             "Cecil, "
             "Aaron, Flora, Tina, Arden, Noel, and Ellen sinned.",
             true),
-        std::make_pair("ab", false), std::make_pair("abab", false),
+        std::make_pair("ab", false),
+        std::make_pair("abab", false),
         std::make_pair("abb", false),
         std::make_pair("A nut for a jarr of tuna.", false),
         std::make_pair("All lets Della call Ed “Stella.”", false),
